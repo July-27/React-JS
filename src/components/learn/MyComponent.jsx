ef95d1c50aea1c91b5986163a4ad7429b439f0b2
Van Toan <vantoan7k@gmail.com>
2025-02-26 23:41:30 +0700

    Chapter5-30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,3 +1,6 @@
+import "./style.css";
+
+//JSX chỉ có 1 parent(1 thẻ HTML trong file)--> dùng fragment
 // arrow function(name must start with upper case character)
 const FakeComponent = () => {
   return <div>fake component</div>;
@@ -5,7 +8,14 @@ const FakeComponent = () => {
 FakeComponent;
 
 const MyComponent = () => {
-  console.log(">>> run my arrow function");
-  return <div>You must take 4 hours a day for study Code FE in 6 months</div>;
+  return (
+    //fragment
+    <>
+      <div>You must take 4 hours a day for study Code FE in 6 months</div>
+      <div className="july" style={{ borderRadius: "10px" }}>
+        July
+      </div>
+    </>
+  );
 };
 export default MyComponent; // chỉ xuất ra 1 component chỉ định
