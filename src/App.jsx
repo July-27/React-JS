3ad11f5b5a34ab07d4ce6f26d90908d04d9d1bca
Van Toan <vantoan7k@gmail.com>
2025-02-27 17:08:15 +0700

    Chapter5-34

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import TodoNew from "./components/todo/TodoNew";
 import LogoReact from "./assets/react.svg";
 
 const App = () => {
+  const name = "July";
+  const age = 21;
+  const data = {
+    country: "VietNam",
+    major: "AI",
+  };
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData name={name} age={age} data={data} />
       <div className="todo-image">
         <img src={LogoReact} className="logo" />
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,28 @@
-const TodoData = () => {
+const TodoData = (props) => {
+  // cách 1
+  // Props là một object {}
+
+  // cách 2
+  //   {
+  //     name: "July",
+  //     age: 21,
+  //     data: {
+  //         country:"VietNam"
+  //         major:"AI"
+  //       }
+  //   }
+
+  // cách 3
+  const { name, age, data } = props;
+  // ý nghĩa của cách trên
+  // const name = props.name;
+  // const age = props.age;
+  // const data = props.data;
+
+  // console.log("check props: ", props);
   return (
     <div className="todo-data">
+      <div> My name is {name} </div>
       <div> Learning React </div>
       <div> Watching Youtube</div>
     </div>
