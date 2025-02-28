6ef1fe7bc06d0ce1d28dc2074719d8c8c4ae0c8b
Van Toan <vantoan7k@gmail.com>
2025-02-28 20:32:50 +0700

    Chapter6-45

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,33 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // import "./index.css";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <div>Login page</div>,
+  },
+  {
+    path: "/regrters",
+    element: <div>Login page</div>,
+  },
+  {
+    path: "/users",
+    element: <div>Users page</div>,
+  },
+  {
+    path: "/products",
+    element: <div>Products page</div>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
